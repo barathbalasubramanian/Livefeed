2df541d8e895f326b2e277a694604e1b059c4c43
barathbalasubramanian <barathkumar.b2411@gmail.com>
2023-08-09 17:27:28 +0530

    send image to python file

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const WebSocket = require('ws');
 const http = require('http');
 const app = express();
 
+const {spawn} = require('child_process');
+
+
 // app.set("view-engine", "ejs");
 app.use(express.static('public'));
 
@@ -13,8 +16,36 @@ wss.on('connection', ws => {
   ws.on('message', message => {
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        console.log('video received successfully..')
-        client.send(message.toString());
+        // console.log('video received successfully..')
+        // client.send(message.toString());
+
+        let dataToSendToPythonScript =  'req.file';
+        // console.log(dataToSendToPythonScript)
+        var detect;
+
+        const python = spawn('python', ['suma.py' , message.toString() ]);
+
+        python.stdout.on('data', function (data) {
+            console.log('Pipe data from python script ...');
+            dataToSend = data.toString();
+            console.log(dataToSend)
+            // let result = dataToSend.match(/\[\s*'([^']*)'\s*\]/)[1];
+            // detect = result
+            // console.log(result);
+            // console.log(result.length);
+
+        });
+
+        python.stderr.on('data', (data) => {
+            console.error(`stderr: ${data}`);
+        });
+
+        python.on('close', (code) => {
+            // console.log(`child process close all stdio with code ${code , detect}`);
+            // res.setHeader('Content-Disposition', `${detect}`);
+            // res.sendFile(filePath)
+        });
+
       }
     });
   });
---
+++ suma.py
@@ -0,0 +1,7 @@
+
+import sys
+
+if len(sys.argv) > 1:
+    dataFromNodeScript = sys.argv[1]
+    # print(dataFromNodeScript)
+print('Hii')
